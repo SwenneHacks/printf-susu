9db963c4b6a9e38acde9c64862178f366ba5546b
Swenne Offerhaus <swofferh@student.codam.nl>
2020-06-22 22:12:09 +0200

    new debuggng round

--- main.c
+++ main.c
@@ -1,12 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: swofferh <swofferh@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/06/22 19:19:09 by swofferh      #+#    #+#                 */
+/*   Updated: 2020/06/22 19:46:18 by swofferh      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "test.h"
 
-int		main(void)
+int	main(void)
 {
 	int a = 0;
 	int b = 0;
-	a = printf(		"[%d%d%d%d]\n", -1, -2, 3, 4);
-	b = ft_printf(	"[%d%d%d%d]\n", -1, -2, 3, 4);
+	a = printf(		"[%-3.1u]\n", 3);
+	b = ft_printf(	"[%-3.1u]\n", 3);
 	printf("\npf |%d|\nft |%d|\n\n", a, b);
-	return(0);
+	return (0);
 }
